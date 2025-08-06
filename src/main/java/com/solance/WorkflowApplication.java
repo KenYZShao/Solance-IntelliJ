98deef7961d01aca284b133108c452ca520ca8ff
Ken Shao <zhoufanxiu1997@gmail.com>
2025-08-06 08:39:11 +0100

    First Commit

--- src/main/java/com/solance/WorkflowApplication.java
+++ src/main/java/com/solance/WorkflowApplication.java
@@ -11,7 +11,7 @@ import java.sql.Connection;
 @SpringBootApplication
 public class WorkflowApplication {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) { //
         SpringApplication.run(WorkflowApplication.class, args);
     }
 
